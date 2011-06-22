5cd8267094b1f5490195c773b4b8a56c8ad1fd7b
Andy Mortimer <aamortimer@gmail.com>
2011-06-22 23:21:19 +0100

    Changed the content type

--- feed.php
+++ feed.php
@@ -99,7 +99,7 @@
   }  
 ?>
 <?php 
-header('Content-type: text/xml'); 
+header('Content-type: application/rss+xml'); 
 header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
